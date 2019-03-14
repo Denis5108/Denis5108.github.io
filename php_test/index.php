d5c1aaac06e5b7db673b51b07dce8a25118e33c7
Denis Garley <gar18056@byu.edu>
2019-03-14 01:56:35 -0600

    Update index.php

--- php_test/index.php
+++ php_test/index.php
@@ -1,3 +1,24 @@
 <?php
-echo "This is my frst php file";
+   session_start();
+
+   $accessToken = $_SESSION('my_access_token_accessToken')
 ?>
+<html>
+<head>
+</head>
+<body>
+
+
+  <?php
+  echo '<p>access token:</p>';
+  echo '<p><code>' .$accessToken. '</code></p>';
+  echo '<br />';
+
+  if ($accessToken != "") {
+    echo '<p>Logged in!</p>';
+  } else {
+    echo '<p><a href="https://github.com/login/oauth/authorize?client_id=0512b3d00db691f699d7>Sign in with github</a></p>"';
+  }
+?>
+</body>
+</html>
